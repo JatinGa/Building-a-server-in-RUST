47b987d4a34b246496a49ffa0ddc6799ec23a745
JatinGa <jatingarg1977@gmail.com>
2025-09-07 18:28:51 +0530

    content from htmlll

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,14 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
 
     stream.read(&mut buffer).unwrap();
-    println!(
-        "Request: {}", 
-        String::from_utf8_lossy(&buffer[..])
+
+    let contents = fs::read_to_string("index.html").unwrap();
+    
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
     );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
